9a149e017c61f997d5ecf69444e717eee754c7e4
denisYordanov-eng <denisivov437@gmail.com>
2025-08-31 14:00:32 +0300

    Made a task.

---
+++ BooleanVariable.java
@@ -0,0 +1,15 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class BooleanVariable {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input.equals("True") ) {
+            System.out.println("Yes");
+        }else if (input.equals("False") ) {
+            System.out.println("No");
+        }
+    }
+}
